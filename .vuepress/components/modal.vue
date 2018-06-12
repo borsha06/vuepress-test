9a682d30b67ce54ffe084bd6f93aa4dd74b51501
borsha06 <tasfia.sharmin1994@gmail.com>
2018-06-13 00:01:48 +0600

    vue-bootstrap

--- .vuepress/components/modal.vue
+++ .vuepress/components/modal.vue
@@ -1,23 +1,20 @@
 <template>
     <div class="modal">
-        <q-btn @click="modal">Do something</q-btn>
+        <b-button @click="modal">I am a Button</b-button>
     </div>
 
 </template>
 <script>
-    import Vue from 'vue';
-    import Quasar from "quasar-framework/dist/quasar.mat.esm";
-    import "quasar-framework/dist/umd/quasar.mat.css";
-    import Swal from 'sweetalert2';
-    import { QBtn } from "quasar-framework/dist/quasar.mat.esm";
 
-    Vue.use(Quasar);
+    import Vue from 'vue'
+    import BootstrapVue from 'bootstrap-vue'
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+    Vue.use(BootstrapVue);
 
     export default{
-        name: "App",
-        components: {
-            QBtn,
-        },
+
 
         data(){
             return {
